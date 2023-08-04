d3b801eb9667ea5e2f402b90f4cc6b998bedf29b
Sarah-2002 <sarah17222002@gmail.com>
2023-08-04 20:44:20 +0300

    added

--- index.js
+++ index.js
@@ -187,7 +187,34 @@ function selectParent(population) {
   }
 
   /*  Method 2 :
+  function selectParent(population) {
+  // Function to perform Tournament Selection for parent selection
+  const tournamentSize = 5; // Adjust the tournament size as needed
 
+  // Select parents using Tournament Selection
+  const parents = [];
+  for (let i = 0; i < populationSize; i++) {
+    // Randomly pick tournamentSize individuals from the population
+    const tournamentParticipants = [];
+    for (let j = 0; j < tournamentSize; j++) {
+      const randomIndex = Math.floor(Math.random() * population.length);
+      tournamentParticipants.push(population[randomIndex]);
+    }
+
+    // Sort the tournament participants based on their fitness (descending order)
+    tournamentParticipants.sort((a, b) => b.fitness - a.fitness);
+
+    // Select the fittest individual from the tournament as a parent
+    parents.push(tournamentParticipants[0]);
+  }
+
+  return parents;
+}
+
+**************************************************************************************************************************************************************************************
+
+
+Method 3 :
 
 
 
